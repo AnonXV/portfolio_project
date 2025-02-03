6c2cbf64344fb1f72aa86d2509b2513b5e174e9c
AnonX <augustinelbecker@gmail.com>
2025-02-02 22:19:53 -0600

    Add more files (more pages, header/footer), style index, update JS

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-//nothing yet
+function changeTopMessage(str) {
+    document.getElementById("topMessage").textContent = str;
+}
+
+function openModal(id) {
+    document.getElementById(id).style.display = "block";
+}
+
+function closeModal(id) {
+    document.getElementById(id).style.display = "none";
+}
+
+function loadHTML(elementId, url, callback) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+            if (callback) callback();
+        });
+}
+
+const messages = {
+    "/index.html": "Home to the X",
+    "/about.html": "About. Just about.",
+    "/legacy.html": "Observe and Learn",
+    "/subscribe.html": "Sub Cause Yes"
+};
+
+function updateTopMessage() {
+    const path = window.location.pathname;
+    const message = messages[path] || "Welcome";
+    changeTopMessage(message);
+}
+
+loadHTML('header', 'header.html', updateTopMessage);
+loadHTML('footer', 'footer.html');
